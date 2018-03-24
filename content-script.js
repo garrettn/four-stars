99cd63a55fd3ca68ebc1f6b47c702552c0753b8f
Garrett Nay <g@garrettnay.com>
2018-03-24 15:11:59 -0600

    Basic word replacement

--- content-script.js
+++ content-script.js
@@ -1,17 +1,35 @@
+const probability = 0.1;
+
+function replaceRandomWord(node) {
+  const words = node.textContent.split(/\W+/).filter(w => w.length > 3);
+
+  if (!words.length) {
+    return;
+  }
+
+  const toReplace = words[Math.floor(Math.random() * words.length)];
+
+  node.textContent = node.textContent.replace(toReplace, '****');
+}
+
 const iterator = document.createNodeIterator(
   document.body,
   NodeFilter.SHOW_TEXT,
-  node =>
-    !/^\s*$/.test(node.data)
-      ? NodeFilter.FILTER_ACCEPT
-      : NodeFilter.FILTER_REJECT
+  node => {
+    if (Math.random() > probability) {
+      return NodeFilter.FILTER_REJECT;
+    }
+
+    if (/^\s*$/.test(node.data)) {
+      return NodeFilter.FILTER_REJECT;
+    }
+
+    return NodeFilter.FILTER_ACCEPT;
+  }
 );
 
 let currentNode;
-let nodes = [];
 
 while ((currentNode = iterator.nextNode())) {
-  nodes.push(currentNode.textContent);
+  replaceRandomWord(currentNode);
 }
-
-console.log(nodes);
